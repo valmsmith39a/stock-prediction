775c1f618a1fcfded959ad0049695b49a55d1bbf
valmsmith39a <valmsmith39a@gmail.com>
2018-11-11 19:48:36 -0800

    Set up basic react frontend app

---
+++ src/components/AppRoot.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AppRoot extends Component {
+
+  render () {
+    return (
+      <div>Basic React App</div>
+    )
+  }
+}
+
+export default AppRoot
